3b9ad7413b5a679292ced3c3cde9ef13b872ae95
sahiljain11 <sahilbot1@gmail.com>
2020-06-08 00:54:15 -0500

    Modularized code so preprocessing is prior

--- create_model.py
+++ create_model.py
@@ -22,7 +22,7 @@ from progress.bar import IncrementalBar
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-number_of_features = 192  # input size
+number_of_features = 195  # input size
 number_of_hidden   = 100  # size of hidden layer
 number_of_gestures = 12   # output size
 sequence_length    = 20   # refers to the amount of timeframes to check
@@ -32,30 +32,74 @@ num_epoch          = 50
 folder_name = ["none", "pinch_in", "pinch_out", "swipe_up", "swipe_down", "swipe_left", "swipe_right",
                "grab2fist", "fist2grab", "peace", "2fingers", "pointing"]
 
-# PREPROCCESSING
-#file_tensors = []
-#for i in range(0, len(folder_name)):
-
 def create_training_tensor(data_file):
-    # construct a matrix tensor that has the proper dimensions
-    # should be num_of_features x max_batch_size
+
+    # count how many lines are in the specific csv file
+    # not a more efficent way tho. sad :(
+    f = open(data_file, "r")
+    file_data = f.readlines()
+
+    portion_of_data = 0.6
+    number_of_lines = len(file_data)
+
+    # ADDED TO ONLY LIKE THE MIDDLE PORTION OF EACH FILE
+    middle = int(math.floor(number_of_lines * 0.5))
+    inner = middle - int(math.floor(number_of_lines * portion_of_data / 2))
+    outer = middle + int(math.ceil(number_of_lines * portion_of_data / 2))
+
     file_contents = []
-    with open(data_file, "r") as file:
-        file.readline()
-        for line in file.readlines():
-            line_array = line.split(",")
-            temp_line = []
-            # ignore frame number and time
-            for i in range (2, len(line_array) - 1):
-                # check for any empty entries
-                if (line_array[i] != "" and line_array[i] != "\n" and line_array[i] != "0"):
-                    temp_line.append(float(line_array[i]))
-            if len(temp_line) == number_of_features:
-                file_contents.append(temp_line)
+
+    #print(str(inner) + " " + str(middle) + " " + str(outer))
+
+    for row_num in range(inner, outer):
+        if row_num == 0:
+            continue
+        # get each row of the csv
+        line = file_data[row_num]
+        
+        # split by comma delimmeter
+        line_array = line.split(",")
+        temp_line = []
+
+        # any weird line with substantially less features
+        if len(line_array) != number_of_features + 3:
+            continue
+
+        # ignore frame number and time
+        for i in range (2, len(line_array) - 1):
+            # check for any empty entries
+            if (line_array[i] != "" and line_array[i] != "\n"):
+                temp_line.append(float(line_array[i]))
+
+        file_contents.append(temp_line)
 
     # convert to a pytorch tensor
     training_tensor = torch.FloatTensor(file_contents)
     return training_tensor
+
+
+# PREPROCCESSING
+file_tensors = [None] * number_of_gestures
+with IncrementalBar("Preprocessing...", max=number_of_gestures) as increment_bar:
+    for i in range(0, number_of_gestures):
+        # navigate to subfolder
+        name = folder_name[i]
+        data_dir = os.path.join(basedir, 'csv_data/' + name + '/')
+        files = os.listdir(data_dir)
+
+        # create tensors from all the training data files
+        file_tensors[i] = []
+
+        for j in range(0, len(files)):
+            # traverse through each file in each sub folder
+            test_string = name + str(j) + ".csv"
+            training_file = os.path.join(data_dir, test_string)
+
+            # convert them to tensors
+            training_tensor = create_training_tensor(training_file)
+            file_tensors[i].append(training_tensor)
+        increment_bar.next()
+    
 
 # LSTM model
 class JarvisLSTM(nn.Module):
@@ -88,21 +132,11 @@ class JarvisLSTM(nn.Module):
 
 def train_model(model, training_data, batch_size, loss_function, optimizer, seq_length, num_features, targets, hidden_dim):
 
-    portion_of_data = 0.6
     running_loss = 0.0
     count = 0
 
-    # ADDED TO ONLY DO LIKE THE MIDDLE PORTION OF EACH FILE
-    iterations = (((len(training_data) - batch_size)) - seq_length) / batch_size
-
-    middle = int(math.floor(iterations * 0.5))
-    inner = middle - int(math.floor(iterations * portion_of_data / 2))
-    outer = middle + int(math.ceil(iterations * portion_of_data / 2))
-
-    # iterate through the portions
-    for j in range(inner, outer):
-
-        i = (j * batch_size) + seq_length
+    # iterate through the data
+    for i in range(seq_length, len(training_data) - batch_size, batch_size):
 
         # convert data into batch_size x num_of_features
         inner_bound = i - seq_length
@@ -150,41 +184,17 @@ def epoch(folder_name, number_of_gestures, batch_size, lstm_model, loss_function
     # traverse through all of the 12 gesture training data
     with IncrementalBar("Training " + str(epoch_num) + "...", max=number_of_gestures) as increment_bar:
         for i in range(0, number_of_gestures):
-            name = folder_name[i]
-            print(name)
-
-            basedir = os.path.abspath(os.path.dirname(__file__))
-            data_dir = os.path.join(basedir, 'csv_data/' + name + '/')
-            files = os.listdir(data_dir)
-            file_hash = {}
-
-            # turn the file data into a hash for O(1)
-            for j in range(0, len(files)):
-                file_hash[files[j]] = 0
-
-            count = 0
-
             # batch_size x seq_length x num_gestures
             target = i * torch.ones(batch_size, number_of_gestures, dtype=torch.long)
 
-            # determine what count number this is
-            test_string = name + str(count) + ".csv"
+            # traverse through each file and train
+            for j in range(0, len(file_tensors[i])):
+                return_loss = train_model(lstm_model, file_tensors[i][j], batch_size, loss_function,
+                                          optimizer, sequence_length, number_of_features, target, hidden_dim)
 
-            return_loss = 0.0
-
-            while True:
-                if test_string in file_hash:
-                    # csv file exists. therefore train with it
-                    training_file = os.path.join(data_dir, test_string)
-                    training_tensor = create_training_tensor(training_file)
-                    return_loss = train_model(lstm_model, training_tensor, batch_size, loss_function,
-                                           optimizer, sequence_length, number_of_features, target, hidden_dim)
-                    count += 1
-                    test_string = name + str(count) + ".csv"
-                else:
-                    break
-            increment_bar.next()
+            # add the loss at the end and increment the progress bar
             avg_total_loss.append(return_loss)
+            increment_bar.next()
             
     print("Loss: " + str((avg_total_loss)))
     increment_bar.finish()
@@ -198,8 +208,6 @@ with IncrementalBar("Epoch...", max=num_epoch, suffix='%(percent).1f%% - %(eta)d
     lstm_model.train()
     for i in range (0, num_epoch):
         epoch(folder_name, number_of_gestures, batch_size, lstm_model, loss_function, optimizer, sequence_length, number_of_features, i, number_of_hidden)
-        #for param in lstm_model.parameters():
-        #    print(param.data)
         bar.next()
 
 STORAGE_PATH = "state_dict_model.pt"
