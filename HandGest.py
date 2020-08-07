5f18b46e4a0fd7c9adc54bc50dd222c3716c1ace
Conrad Li <conradliste@gmail.com>
2020-08-06 23:28:42 -0500

    Updated HandGest to match with last commit

--- HandGest.py
+++ HandGest.py
@@ -3,10 +3,20 @@ import time
 import requests
 import pyautogui
 from os import environ
+from PySide2.QtCore import (
+    QCoreApplication,
+    QEvent,
+    QObject,
+    QPointF,
+    Qt,
+    QTimer,
+    QUrl,
+    Slot,
+)
+from PySide2.QtGui import QGuiApplication, QMouseEvent, QCursor
+from PySide2.QtQuick import QQuickItem, QQuickView
 from PySide2 import QtWidgets as qtw
-from PySide2 import QtGui as qtg
 from PySide2 import QtCore as qtc
-from PySide2 import QtQuick as qtq
 from PySide2 import QtQml as qtm
 
 class HandGest(qtc.QObject):
@@ -25,44 +35,48 @@ class HandGest(qtc.QObject):
         self.z = 0
         self.old_gest = 0
         self._gest = 0
-        self.cursor = qtg.QCursor()
+        self.cursor = QCursor()
     
     # Sets the current gesture data
-    @qtc.Slot()
+    @qtc.Slot(result='QVariant')
     def set_gest_data(self):
-        res = requests.post("http://127.0.0.1:5000/determine-gesture/", json=send).json()
+        
+        res = requests.post("http://127.0.0.1:5000/determine-gesture/").json()
         self.old_gest = self._gest
         self.setGest(res['gesture'])
-        '''self.setX(res['x'])
+        self.setX(res['x'])
         self.setY(res['y'])
-        self.setZ(res['z'])'''
+        self.setZ(res['z'])
         self.x = res['x']
         self.y = res['y']
         self.z = res['z']
-        return
+        
+        return QPointF(self.x, self.y)
     
-    @qtc.Slot()
+    @qtc.Slot(result='QVariant')
     def setMouse(self):
+        # Set the cursor position
         self.cursor.setPos(self.x, self.y)
-
+        
         # Case for mouse up after performing a mouse down
         if self._gest == 0 and (self.old_gest == 1 or self.old_gest == 2):
             if(self.old_gest == 2):
                 pyautogui.keyUp("alt")
-            pyautogui.mouseUp()
-            
+
+        # Case for mouse down and drag
+        elif self._gest == 2 and self.old_gest != 2:
+            pyautogui.keyDown("alt")
+        
+        '''
         # Case for clicking
         elif self._gest == 3 and self.old_gest != 3:
-            pyautogui.click()
+        #    pyautogui.click()
 
         # Case mouse down
         elif self._gest == 1 and self.old_gest != 1:
-            pyautogui.mouseDown()
-        
-        # Case for mouse down and drag
-        elif self._gest == 2 and self.old_gest != 2:
-            pyautogui.keyDown("alt") 
-            pyautogui.mouseDown()
+        #   pyautogui.mouseDown()
+        '''
+        return [self.gest, self.old_gest]
 
     @qtc.Property(int, notify=gestChanged)
     def gest(self):
@@ -99,22 +113,70 @@ class HandGest(qtc.QObject):
         self.y = newY
         self.handYChanged.emit()
     
-    @qtc.Property(float, notify=handZChanged)
-    def handZ(self):
-        return self.z
+    @Slot(QQuickItem, Qt.MouseButton)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier, QPointF)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier, QPointF, int)
+    def mouseClick(self, item, button, modifier=Qt.NoModifier, pos=QPointF(), delay=-1):
+        self.mousePress(item, button, modifier, pos, delay)
+        self.mouseRelease(item, button, modifier, pos, 2 * delay)
 
-    @handZ.setter
-    def setZ(self, newZ):
-        # Do not emit a changed signal if the icon is the same
-        if self.z == newZ:
-            return
-        self.z = newZ
-        self.handZChanged.emit()
+    @Slot(QQuickItem, Qt.MouseButton)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier, QPointF)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier, QPointF, int)
+    def mousePress(self, item, button, modifier=Qt.NoModifier, pos=QPointF(), delay=-1):
+        self._send_mouse_events(
+            QEvent.MouseButtonPress, item, button, modifier, pos, delay
+        )
+
+    @Slot(QQuickItem, Qt.MouseButton)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier, QPointF)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier, QPointF, int)
+    def mouseRelease(
+        self, item, button, modifier=Qt.NoModifier, pos=QPointF(), delay=-1
+    ):
+        self._send_mouse_events(
+            QEvent.MouseButtonRelease, item, button, modifier, pos, delay
+        )
+
+    @Slot(QQuickItem, Qt.MouseButton)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier, QPointF)
+    @Slot(QQuickItem, Qt.MouseButton, Qt.KeyboardModifier, QPointF, int)
+    def mouseDClick(
+        self, item, button, modifier=Qt.NoModifier, pos=QPointF(), delay=-1
+    ):
+        self.mousePress(item, button, modifier, pos, delay)
+        self.mouseRelease(item, button, modifier, pos, 2 * delay)
+        self.mousePress(item, button, modifier, pos, 3 * delay)
+        self._send_mouse_events(
+            QEvent.MouseButtonDblClick, item, button, pos, 4 * delay
+        )
+        self.mouseRelease(item, button, modifier, pos, 2 * delay)
+
+    def _send_mouse_events(self, type_, item, button, modifier, pos, delay):
+        window = item.window()
+        if pos.isNull():
+            pos = item.boundingRect().center()
+        sp = item.mapToScene(pos).toPoint()
+        event = QMouseEvent(
+            type_, pos, window.mapToGlobal(sp), button, button, modifier
+        )
+        if delay < 0:
+            delay = 0
+
+        def on_timeout():
+            QCoreApplication.instance().notify(window, event)
+
+        QTimer.singleShot(delay, on_timeout)
+    
 
 if __name__ == '__main__':
 
     # Initializes the app, engine, and classes 
-    app = qtg.QGuiApplication(sys.argv)
+    app = QGuiApplication(sys.argv)
     engine = qtm.QQmlApplicationEngine()
     root_context = engine.rootContext()
     
